5a68889028434b25bf8c823426b1fb12e474cf8a
Bell <skyfall.kay.team@gmail.com>
2022-06-15 09:43:47 +0200

    fix button test
    Fix du button de test

--- VectoR/Assets/Scripts/buttonTestScript.cs
+++ VectoR/Assets/Scripts/buttonTestScript.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class buttonTestScript : MonoBehaviour
 {
     public GameObject coordinateSystem;
-
+    public GameObject tm;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tm = GameObject.Find("ToolManager");
     }
 
     // Update is called once per frame
@@ -23,14 +23,14 @@ public class buttonTestScript : MonoBehaviour
     // Test method of creation of a vector with given coordinates
     public void test2pointVector()
     {
-        VectorTool vt = GetComponent<VectorTool>();
+        VectorTool vt = tm.GetComponent<VectorTool>();
         vt.createVectorFrom2points(coordinateSystem, new Vector3(0, 0, 0), new Vector3(1, 2, 1));
     }
 
     // Test method of creation of a vector from nothing
     public void testnopointVector()
     {
-        VectorTool vt = GetComponent<VectorTool>();
+        VectorTool vt = tm.GetComponent<VectorTool>();
         vt.createFromNothing(coordinateSystem);
     }
 }
